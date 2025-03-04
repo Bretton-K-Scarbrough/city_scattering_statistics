4e3d831d3b3e68d6820f14ec8be2dc0058cffcfb
Bretton-K-Scarbrough <bretton.scarbrough@gmail.com>
2025-03-04 16:57:00 -0600

    created TV static

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from propagators import propFraunhofer, propTF_Fresnel
+from propagators import propTF_Fresnel
 import numpy as np
 import matplotlib.pyplot as plt
 import os
@@ -6,13 +6,13 @@ import os
 # Physical Constants
 lam = 532e-9
 L = 500e-3
-N = 1000
+N = 2000
 dx = L / N
 x = np.linspace(-L / 2, L / 2 - dx, N)
 X, Y = np.meshgrid(x, x)
 
 # Initialize RNG
-mu, sigma = 0, 10
+mu, sigma = 0, 1.9
 
 
 if __name__ == "__main__":
@@ -31,8 +31,7 @@ if __name__ == "__main__":
         print(f"An error occurred: {e}")
 
     # Create a planewave
-    A = np.ones_like(X, dtype="complex")
-    planewave = A * np.exp(1j * (2 * np.pi) / lam)
+    planewave = np.exp(-np.pi * (X**2 + Y**2) / 100e-3)
 
     # Generate a random phase distribution
     rand_phi = np.random.normal(mu, sigma, planewave.shape)
@@ -41,19 +40,20 @@ if __name__ == "__main__":
 
     propagated_field = propTF_Fresnel(scattered_field, L, lam, 20 * lam)
 
+    loops = 100
     plt.figure()
-    plt.imshow(np.abs(propagated_field) ** 2)
-    plt.show()
-
-    loops = 3
     for i in range(loops):
         rand_phi = np.random.normal(mu, sigma, planewave.shape)
-        scattered_filed = propagated_field * np.exp(rand_phi)
-        propagated_field = propTF_Fresnel(scattered_field, L, lam, 20 * lam)
-        plt.imshow(np.abs(propagated_field) ** 2)
-        plt.title(f"Loop {i}")
-        plt.show()
-
+        scattered_field = propagated_field * np.exp(1j * rand_phi)
+        propagated_field = propTF_Fresnel(scattered_field, L, lam, 100 * lam)
+        prop_intensity = np.abs(propagated_field) ** 2
+        plt.clf()
+        # plt.hist(prop_intensity.flatten(), bins=100, alpha=0.3, label=f"loop {i}")
+        # plt.legend()
+        plt.imshow(np.angle(propagated_field), cmap="rainbow")
+        plt.title(f"loop {i}")
+        plt.pause(1)
+    plt.show()
 
 # TODO:
 # - Create a plane wave
